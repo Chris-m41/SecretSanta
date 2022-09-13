0246b2a677bc671b859a01396c9b8cac8dc6ebda
Christian Martinez <chrism577@yahoo.com>
2022-09-13 16:04:58 -0500

    Initial Commit

---
+++ SecretSanta/SecretSantaApp.swift
@@ -0,0 +1,17 @@
+//
+//  SecretSantaApp.swift
+//  SecretSanta
+//
+//  Created by Christian Martinez on 9/13/22.
+//
+
+import SwiftUI
+
+@main
+struct SecretSantaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
